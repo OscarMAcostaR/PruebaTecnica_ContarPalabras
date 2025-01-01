788bc5eb1bc925fe6aea28c823d9eb51ec107b7c
Oscar Manuel Acosta Rios <oscar96261@gmail.com>
2025-01-01 17:15:16 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnica_ContarPalabras/Program.cs
@@ -0,0 +1,23 @@
+﻿// Prueba Tecnica contar palabras
+
+//inyeccion de librerias o de dependencias para expresiones regulares
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Ingrese una cadena");
+string cadena = Console.ReadLine();
+
+int contador = 0;
+
+// El metodo Trim() elimina los espacios en blanco al principio y al final de la cadena
+// El metodo Split() divide la cadena en palabras separadas por un espacio
+// Las expresiones regulares 
+// \s indica un espacio 
+// Como le indico que quiero que sea 1 o mas espacios \s+
+// Nada o algo \s*
+// lo siguiente indica que si hay un espacio o mas los conviente todo a un espacio simple @"\s", " "
+
+cadena = Regex.Replace(cadena, @"\s+", " ").Trim();
+var palabras = cadena.Split(' ');
+contador = palabras.Length;
+
+Console.WriteLine($"La cantidad de palabras es: {contador}");
